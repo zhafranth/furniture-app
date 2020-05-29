318de958999fdc4f0d103b700fc4c4736751af13
Zhafranth <zhafranth@gmail.com>
2020-05-29 16:47:04 +0700

    add BrandTextIcon Component

---
+++ src/components/IconText.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from "elements/Button";
+
+export default function IconText() {
+  return (
+    <Button type="link" className="brand-text-icon" href="/">
+      Furni<span className="text-gray-900">House</span>
+    </Button>
+  );
+}
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,15 +2,14 @@ import React, { Component } from "react";
 
 // component
 import Button from "elements/Button/";
+import BrandTextIcon from "components/IconText";
 
 export default class HomePage extends Component {
   render() {
     return (
-      <div>
+      <div className="container mx-5">
         <h1>Hello From Home Page</h1>
-        <Button isPrimary type="link">
-          ini tombol
-        </Button>
+        <BrandTextIcon />
       </div>
     );
   }
