916b1b0ce9422347f41113c73a1834deab25c9b8
Zhafranth <zhafranth@gmail.com>
2020-06-13 19:41:46 +0700

    Finish Error Page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,11 +1,52 @@
 import React, { Component } from "react";
+import Fade from "react-reveal/Fade";
+
+// Component
+import Button from "elements/Button";
+import Header from "components/Header";
+import Footer from "components/Footer";
+
+// Image
+import errorPage from "assets/images/not-found-page.png";
 
 export default class ErrorPage extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello from Error Page</h1>
-      </div>
+      <>
+        <Header {...this.props} />
+        <div className="container text-center" style={{ margin: " 10vh auto" }}>
+          <div className="row my-5">
+            <Fade top>
+              <div className="col-md-12">
+                <img
+                  src={errorPage}
+                  alt="error page"
+                  width="50%"
+                  height="auto"
+                />
+              </div>
+              <div className="col">
+                <h5
+                  style={{
+                    textTransform: "uppercase",
+                    fontWeight: 700,
+                    margin: "10px 0",
+                    fontSize: "2rem",
+                  }}
+                >
+                  The Page Was Lost
+                </h5>
+              </div>
+            </Fade>
+          </div>
+          <Fade bottom>
+            <Button href="/" type="link" className="btn">
+              Back to Home
+            </Button>
+          </Fade>
+        </div>
+        <Footer />
+      </>
     );
   }
 }
